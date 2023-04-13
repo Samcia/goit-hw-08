f1e3a27f1629d2098a39141b8a8423d2b00be383
Samcia <samantta689@gmail.com>
2023-04-13 13:33:13 +0200

    com

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
