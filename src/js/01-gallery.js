68037d543a5b11b805ae71a85455501082c8195d
Samcia <samantta689@gmail.com>
2023-04-13 12:59:49 +0200

    first update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,22 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+// Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+
+for (const img of galleryItems) {
+  const listEl = `<li>
+  <a class="gallery__item" href=${img.original}>
+    <img class="gallery__image" src=${img.preview} 
+    alt=${img.description} />
+  </a></li>`;
+  galleryEl.insertAdjacentHTML('beforeend', listEl);
+}
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(galleryItems);
