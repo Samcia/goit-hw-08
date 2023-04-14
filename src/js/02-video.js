ad98d91ae3bcd7502ff06210151087fadaa4416a
Samcia <samantta689@gmail.com>
2023-04-14 09:38:37 +0200

    task 2 com

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const saveVideoTime = throttle(
+  data => localStorage.setItem('videoplayer-current-time', data.seconds),
+  1000
+);
+player.on('timeupdate', saveVideoTime);
+
+const updateDelay = throttle(seconds => {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
+
+const updateTime = () => video.getCurrentTime().then(updateDelay);
